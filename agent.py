25690cd76572d7501f3fc24c10717388623cd5d3
aceniccola <andrewsamce@me.com>
2024-01-04 13:46:55 -0500

    agents.py init

--- agent.py
+++ agent.py
@@ -1,1 +1,31 @@
+import torch as pt
+import random as rd 
+import numpy as np 
+import params as PARAMS
 
+
+class Agent:
+
+    def __init__(self) -> None:
+        self.memory = ...
+        self.state = ...
+        self.epsilon = PARAMS.EPSILON
+        self.alpha = PARAMS.ALPHA
+        self.beta = ...
+        self.gamma = ...
+        pass
+
+    def move(self):
+        pass
+
+    def remember(self):
+        pass
+    
+    def train_stm(self):
+        pass
+
+    def train_ltm(self):
+        pass
+
+    def reward(self):
+        pass
