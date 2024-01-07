272f9090bb95464acef401fafbb5d423bbd3221f
aceniccola <andrewsamce@me.com>
2024-01-07 14:54:31 -0500

    model

--- model.py
+++ model.py
@@ -33,18 +33,18 @@ class Trainer:
         self.criterion = nn.MSELoss()
 
     def train_step(self, state, action, reward, next_state, done):
-        state = torch.tensor(state, dtype=torch.float)
-        next_state = torch.tensor(next_state, dtype=torch.float)
-        action = torch.tensor(action, dtype=torch.long)
-        reward = torch.tensor(reward, dtype=torch.float)
+        state = pt.tensor(state, dtype=pt.float)
+        next_state = pt.tensor(next_state, dtype=pt.float)
+        action = pt.tensor(action, dtype=pt.long)
+        reward = pt.tensor(reward, dtype=pt.float)
         # (n, x)
 
         if len(state.shape) == 1:
             # (1, x)
-            state = torch.unsqueeze(state, 0)
-            next_state = torch.unsqueeze(next_state, 0)
-            action = torch.unsqueeze(action, 0)
-            reward = torch.unsqueeze(reward, 0)
+            state = pt.unsqueeze(state, 0)
+            next_state = pt.unsqueeze(next_state, 0)
+            action = pt.unsqueeze(action, 0)
+            reward = pt.unsqueeze(reward, 0)
             done = (done, )
 
         # 1: predicted Q values with current state
@@ -54,9 +54,9 @@ class Trainer:
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                Q_new = reward[idx] + self.gamma * pt.max(self.model(next_state[idx]))
 
-            target[idx][torch.argmax(action[idx]).item()] = Q_new
+            target[idx][pt.argmax(action[idx]).item()] = Q_new
     
         # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
         # pred.clone()
